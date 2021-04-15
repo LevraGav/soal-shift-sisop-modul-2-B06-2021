833debf137c5b0860c5c9ca7a0d6ca39df66c34b
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-04-15 15:41:53 +0700

    Update Soal3.c

--- Soal3/Soal3.c
+++ Soal3/Soal3.c
@@ -1,16 +1,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
- 
+#include <stdlib.h>
+#include <sys/wait.h>
 int main(int argc, char* argv[]) {
-  if(strcmp(argv[1],"-z")==0){}
-  if(strcmp(argv[1],"-x")==0){}
+
 
   pid_t pid, sid;        // Variabel untuk menyimpan PID
  
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
         timern = localtime(&now);
         char StrTime[100];
         strftime(StrTime, 100, "%Y-%m-%d_%X", timern);
-        char *arg[] = {"mkdir",StrTime,NULL} 
+        char *arg[] = {"mkdir",StrTime,NULL}; 
 
         pid_t c1 = fork();
         if (c1 < 0) {
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
         if (c1 == 0) {
           execv("/bin/mkdir",arg);
         }
-        while(wait(&Tmkdir>0));
+        while((wait(&Tmkdir))>0);
         pid_t c2 = fork();
         if (c2 < 0) {
           exit(EXIT_FAILURE);
@@ -77,16 +77,19 @@ int main(int argc, char* argv[]) {
             char FStrTime[100];
             int epoch = (int)now % 1000;
             epoch += 50;
-            char link2[4] = atoi(epoch);
-            strftime(FStrTime, 100, "%Y-%m-%d_%X", timern);
+            //char link2[4];
+            //itoa(epoch,link2,10);
+            strftime(FStrTime, 100, "%Y-%m-%d_%X", ftimern);
             char link[100] = "https://picsum.photos/";
-            strncat(link, &link2);
+            *link = sprintf(link,"%d",epoch);
+            //strcat(link, link2);
             pid_t c3 = fork();
             if (c3<0) {
               exit(EXIT_FAILURE);
             }
             if (c3==0) { 
               char *arg[] = {"wget",link,"-O",FStrTime,NULL};
+              execv("/usr/bin/wget",arg);
             } 
             sleep(5);
           }
