3e051f6c949a4d0a071942df0937e83946fb4025
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-04-17 17:45:37 +0700

    rapihin + debug fork

--- Soal3/Soal3.c
+++ Soal3/Soal3.c
@@ -1,4 +1,3 @@
-
 #define _POSIX_SOURCE
 #include <signal.h>
 #include <sys/types.h>
@@ -12,156 +11,104 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-int signals;
+
+int signals = 1;
 
 void orphan(int signum) {
-    signals = 0;
+    signals =0;
 }
 
-int main(int argc, char* argv[]) {
-    if(strcmp(argv[1],"-z")==0){
+int main(int argc, char *argv[]) {
+    if (strcmp(argv[1], "-z") == 0)
+    {
         FILE *ptr2 = NULL;
-        ptr2 = fopen("Killer.sh","w");
-        fputs("#!/bin/bash\nkillall soal3\n rm Killer.sh",ptr2);
+        ptr2 = fopen("Killer.sh", "w");
+        fputs("#!/bin/bash\nkillall soal3\n rm Killer.sh", ptr2);
         fclose(ptr2);
     }
-    if(strcmp(argv[1],"-x")==0){ 
+    if (strcmp(argv[1], "-x") == 0)
+    {
         FILE *ptr2 = NULL;
-        ptr2 = fopen("Killer.sh","w");
-        fputs("#!/bin/bash\nkillall -15 soal3\n rm Killer.sh",ptr2);
+        ptr2 = fopen("Killer.sh", "w");
+        fputs("#!/bin/bash\nkillall -15 soal3\n rm Killer.sh", ptr2);
         fclose(ptr2);
-        signal(SIGTERM,orphan);
-     }
-  pid_t pid, sid;        // Variabel untuk menyimpan PID
- 
-  pid = fork();     // Menyimpan PID dari Child Process
- 
-  /* Keluar saat fork gagal
-  * (nilai variabel pid < 0) */
-  if (pid < 0) {
-    exit(EXIT_FAILURE);
-  }
- 
-  /* Keluar saat fork berhasil
-  * (nilai variabel pid adalah PID dari child process) */
-  if (pid > 0) {
-    exit(EXIT_SUCCESS);
-  }
- 
-  umask(0);
- 
-  sid = setsid();
-  if (sid < 0) {
-    exit(EXIT_FAILURE);
-  }
- 
-  //if ((chdir("/")) < 0) {
-  //  exit(EXIT_FAILURE);
-  //}
- 
- 
-  //close(STDIN_FILENO);
-  //close(STDOUT_FILENO);
-  //close(STDERR_FILENO);
-    signals = 1 ;
-    while (signals) { 
-        //printf("AA\n");
+        signal(SIGTERM, orphan);
+    }
+    pid_t pid, sid;
+    pid = fork();
+    if (pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    umask(0);
+    sid = setsid();
+    if (sid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    while (signals) {
         time_t now;
         time(&now);
         struct tm *timern;
         timern = localtime(&now);
         char StrTime[100];
         strftime(StrTime, 100, "%Y-%m-%d_%X", timern);
-        char *arg[] = {"mkdir",StrTime,NULL}; 
-
-        pid_t c1 = fork();
-        if (c1 < 0) {
-          exit(EXIT_FAILURE);
-        } 
-        if (c1 == 0) {
-          execv("/bin/mkdir",arg);
+        if (fork()==0) {
+            char *arg[] = {"mkdir", StrTime, NULL};
+            execv("/bin/mkdir", arg);
         }
-        printf("OTW LURR \n\n\n\n\n");
         sleep(1);
-        pid_t c2 = fork();
-        if (c2 < 0) {
-          exit(EXIT_FAILURE);
-        } 
-        if (c2 == 0) {
-            // *arg[] = {"chdir",StrTime,Null};
-            // execv(/)
+
+        if (fork()==0) {
             chdir(StrTime);
-            for (int i = 0; i < 10; i++) {
-            time_t fnow;
-            time(&fnow);
-            struct tm ftimern;
-            ftimern = *localtime(&fnow);
-            ftimern.tm_sec-=1;
-            mktime(&ftimern);
-            char FStrTime[100];
-            int epoch = (int)fnow % 1000;
-            epoch += 50;
-            //printf("%d\n\n\n\n\n",epoch);
-            //char link2[4];
-            //itoa(epoch,link2,10);
-            strftime(FStrTime, 100, "%Y-%m-%d_%X", &ftimern);
-            char link[100];
-            sprintf(link,"https://picsum.photos/%d",epoch);
-            printf("%s\n",link);
-            pid_t c3 = fork();
-            if (c3<0) {
-              exit(EXIT_FAILURE);
+            for (int i = 0;i<10;i++) {
+                time_t fnow;
+                time(&fnow);
+                struct tm ftimern;
+                ftimern = *localtime(&fnow);
+                ftimern.tm_sec -= 1;
+                mktime(&ftimern);
+                char FStrTime[100];
+                int epoch = (int)fnow % 1000;
+                epoch += 50;
+                strftime(FStrTime, 100, "%Y-%m-%d_%X", &ftimern);
+                char link[100];
+                sprintf(link, "https://picsum.photos/%d", epoch);
+                if (fork()==0) {
+                    char *arg[] = {"wget", link, "-O", FStrTime, NULL};
+                    execv("/usr/bin/wget", arg);
+                }
+                sleep(5);
             }
-            if (c3==0) {
-              char *arg[] = {"wget",link,"-O",FStrTime,NULL};
-              execv("/usr/bin/wget",arg);
-              //printf("%d\n",epoch);
-            } 
-            sleep(5); 
-          
             char status[] = {"Download Success"};
-            for (int i=0; i<strlen(status);i++) {
-                if (status[i]!=32) {     
-                    status[i]+=5;
-                    if (status[i]>90&&status[i]<97) {
-                        status[i]-=26;
+            for (int i = 0; i < strlen(status); i++)
+            {
+                if (status[i] != 32){
+                    status[i] += 5;
+                    if (status[i] > 90 && status[i] < 97){
+                        status[i] -= 26;
                     }
-                    if (status[i]>122) {
-                        status[i]-=26;
-                    }      
+                    if (status[i] > 122){
+                        status[i] -= 26;
+                    }
                 }
             }
             FILE *ptr = NULL;
-            ptr= fopen("Status.txt","w");
+            ptr = fopen("Status.txt", "w");
             fputs(status, ptr);
-            fclose(ptr); 
+            fclose(ptr);
             chdir("..");
-            int wait3;
-            pid_t c4 = fork();
-            if (c4<0) {
-                exit(EXIT_FAILURE);
-            }
-            if (c4==0) {
-                char abc[30];
-                strcpy(abc,StrTime);
-                strcat(abc,".zip");
-                //printf("%s\n%s\n\n",StrTime,abc);
-                char *arg[] = {"zip",abc,"-r",StrTime,NULL};
-                execv("/usr/bin/zip",arg);
-            }
-            while(wait(&wait3)>0);
-            pid_t c7 = fork();
-            if (c7<0) {
-                exit(EXIT_FAILURE);
-            }
-            if (c7==0) {
-                char *arg[] = {"rm","-r",StrTime,NULL};
-                execv("/usr/bin/rm",arg);
-            } 
+            char abc[30];
+            strcpy(abc, StrTime);
+            strcat(abc, ".zip");
+            //printf("%s\n%s\n\n",StrTime,abc);
+            char *arg[] = {"zip", abc, "-rm", StrTime, NULL};
+            execv("/usr/bin/zip", arg);
         }
-        if (signals ==0) {
-            break;
-        }
-        else sleep(39);
-    }
+        sleep(39);
+    }   
 }
