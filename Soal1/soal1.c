e750861439df98e27f56473c7a620e91e5dfe562
Faisal Reza <7farema@gmail.com>
2021-04-25 00:53:23 +0700

    Update soal1.c

--- Soal1/soal1.c
+++ Soal1/soal1.c
@@ -1,29 +1,22 @@
-#include <string.h>
-#include <stdio.h>
-#include <wait.h>
-#include <stdlib.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <dirent.h>
-#include <time.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <wait.h>
+#include <dirent.h>
 
 time_t my_time;
 struct tm * timeinfo; 
 
-int persiapan(){
-    int hour = 16;
-
-    time (&my_time);
-    timeinfo = localtime (&my_time);
-    if(timeinfo->tm_mday == 9 && timeinfo->tm_mon+1 == 4 && timeinfo->tm_hour == hour && timeinfo->tm_min == 22){
-        return 1;
-    }
-    return 0;
-}
-
-int lopyu(){
+int cektanggal(int persiapan){
     int hour = 22;
+    if(persiapan == 1) hour = 22-6;
 
     time (&my_time);
     timeinfo = localtime (&my_time);
@@ -33,179 +26,23 @@ int lopyu(){
     return 0;
 }
 
-void mkfolder(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        char *argv[] = {"mkdir", "-p", "Pyoto", NULL};
-        execv("/bin/mkdir", argv);
-    }else{
-        id_anak = fork();
-        if (id_anak == 0){
-            char *argv[] = {"mkdir", "-p", "Fylm", NULL};
-            execv("/bin/mkdir", argv);
-        }else{
-            char *argv[] = {"mkdir", "-p", "Musyik", NULL};
-            execv("/bin/mkdir", argv);
-        }
+void execute(char **args){
+    int pid = fork();
+    int status;
+    if(pid == 0){
+        execvp(args[0], args);
     }
-}
 
-void unduh(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        char *argv[] = {"wget", "-q", "--no-check-certificate", "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "Foto_for_Stevany.zip", NULL};
-        execv("/usr/bin/wget", argv);
-    }else{
-        id_anak = fork();
-        if (id_anak == 0){
-            char *argv[] = {"wget", "-q", "--no-check-certificate", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "Film_for_Stevany.zip", NULL};
-            execv("/usr/bin/wget", argv);
-        }else{
-            char *argv[] = {"wget", "-q", "--no-check-certificate", "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "-O", "Musik_for_Stevany.zip", NULL};
-            execv("/usr/bin/wget", argv);
-        }
-    }
-}
-
-void kulitmanggis(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        char *argv[] = {"unzip", "-qq", "Foto_for_Stevany.zip", "-d", "/home/nor/sisop/shift2/", NULL};
-        execv("/usr/bin/unzip", argv);
-    }else{
-        id_anak = fork();
-        if (id_anak == 0){
-            char *argv[] = {"unzip", "-qq", "Film_for_Stevany.zip", "-d", "/home/nor/sisop/shift2/", NULL};
-            execv("/usr/bin/unzip", argv);
-        }else{
-            char *argv[] = {"unzip", "-qq", "Musik_for_Stevany.zip", "-d", "/home/nor/sisop/shift2/", NULL};
-            execv("/usr/bin/unzip", argv);
-        }
-    }
-}
-
-void salin(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        char *argv[] = {"cp", "-r", "FOTO/.", "Pyoto/", NULL};
-        execv("/bin/cp", argv);
-    }else{
-        id_anak = fork();
-        if (id_anak == 0){
-            char *argv[] = {"cp", "-r", "FILM/.", "Fylm/", NULL};
-            execv("/bin/cp", argv);
-        }else{
-            char *argv[] = {"cp", "-r", "MUSIK/.", "Musyik/", NULL};
-            execv("/bin/cp", argv);
-        }
-    }
-}
-
-void hapus(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/FOTO", NULL};
-        execv("/bin/rm", argv);
-    }else{
-        id_anak = fork();
-        if (id_anak == 0){
-            char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/FILM", NULL};
-            execv("/bin/rm", argv);
-        }else{
-            char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/MUSIK", NULL};
-            execv("/bin/rm", argv);
-        }
-    }
-}
-
-void hbd(){
-    pid_t id_anak;
-
-    id_anak = fork();
-    if (id_anak == 0){
-        id_anak = fork();
-        if (id_anak == 0){
-            id_anak = fork();
-            if (id_anak == 0){
-                id_anak = fork();
-                if (id_anak == 0){
-                    id_anak = fork();
-                    if (id_anak == 0){}
-                    else{
-                        char *argv[] = {"zip", "-qr", "Lopyu_Stevany.zip", "Pyoto", "Fylm", "Musyik", NULL};
-                        execv("/usr/bin/zip", argv);
-                    }
-                }else{
-                    sleep(5);
-                    char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/Pyoto", NULL};
-                    execv("/bin/rm", argv);
-                }
-            }else{
-                sleep(5);
-                char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/Fylm", NULL};
-                execv("/bin/rm", argv);
-            }
-        }else{
-            sleep(5);
-            char *argv[] = {"rm", "-r", "/home/nor/sisop/shift2/Musyik", NULL};
-            execv("/bin/rm", argv);
-        }
-    }
-}
-
-void tminus6(){
-    pid_t id_anak;
-    // id_anak = fork();
-    // int status;
-    // if (id_anak == 0){
-        id_anak = fork();
-        int status;
-        if (id_anak == 0){
-            id_anak = fork();
-            int status;
-            if (id_anak == 0){
-                id_anak = fork();
-                int status;
-                if (id_anak == 0){
-                    id_anak = fork();
-                    if (id_anak == 0){
-                        mkfolder();
-                    }else{
-                        unduh();
-                    }
-                }else{
-                    while(wait(&status) > 0);
-                    sleep(5); //5 sec
-                    kulitmanggis();
-                }
-            }else{
-                while(wait(&status)>0);
-                sleep(5);
-                salin();
-            }
-        }else{
-            while(wait(&status)>0);
-            sleep(5);
-            hapus();
-        }
-    // }else{
-    //     while(wait(&status)>0);
-    //     sleep(21600);
-    //     hbd();
-    // }
+    while(wait(&status) > 0);
 }
 
 int main(){
+    char *foldery[3] = {"Musyik/", "Pyoto/", "Fylm/"};
+    char *folderbold[3] = {"MUSIK/", "FOTO/", "FILM/"};
+    char *link[3] = {"https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download"};
+    char *nama_terunduh[3] = {"Musik_for_Stevany.zip","Film_for_Stevany.zip","Foto_for_Stevany.zip"};
+    char working_dir[] = "/home/nor/sisop/shift2/";
+
     int terunduh=0, terzip=0;
 
     pid_t pid, sid;
@@ -217,23 +54,94 @@ int main(){
     umask(0);
 
     sid=setsid();
+    if(sid < 0) exit(EXIT_FAILURE);
+    if((chdir("/home/nor/sisop/shift2")) < 0) exit(EXIT_FAILURE);
 
-    // if ((chdir("/home/nor/sisop/shift2")) < 0) {
-    //     exit(EXIT_FAILURE);
-    // }
-    // if(sid < 0) exit(EXIT_FAILURE);
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
+
     while(1){
-        if(persiapan() && terunduh==0){
-            tminus6();
+        if(cektanggal(1) && terunduh==0){ // e
+            // a
+            for(int i=0; i<3; i++){
+                char *args[] = {"mkdir", foldery[i], NULL};
+                execute(args);
+            }
+
+            // b
+            for(int i=0; i<3; i++){
+                if(fork() == 0){
+                    char *argv[] = {"wget", "-q","--no-check-certificate", link[i], "-O", nama_terunduh[i], NULL};
+                    execv("/usr/bin/wget", argv);
+                }
+            }
+
+            // tunggu b selesai
+            for(int i=0; i<3; i++){
+                int status;
+                pid_t pid = wait(&status);
+                waitpid(pid, &status, WUNTRACED);
+            }
+
+            // c
+            for(int i=0; i<3; i++){
+                if(fork() == 0){
+                    char *argv[] = {"unzip", "-q", nama_terunduh[i], "-d", "", NULL};
+                    execv("/usr/bin/unzip", argv);
+                }
+            }
+
+            // tunggu c selesai
+            for(int i=0; i<3; i++){
+                int status;
+                pid_t pid = wait(&status);
+                waitpid(pid, &status, WUNTRACED);
+            }
+
+            // d
+            for(int i=0; i<3; i++){
+                DIR *dp;
+                struct dirent *ep;
+                char curr_dir[100]; sprintf(curr_dir, "%s%s", working_dir, folderbold[i]);
+                char move_dir[100]; sprintf(move_dir, "%s%s", working_dir, foldery[i]);
+                dp = opendir(curr_dir);
+
+                if(dp != NULL){
+                    while((ep = readdir(dp))){
+                        if(strcmp(ep->d_name, ".") != 0 || strcmp(ep->d_name, "..") != 0){
+                            char file_to_move[355]; sprintf(file_to_move, "%s%s", curr_dir, ep->d_name);
+                            char file_move_to[355]; sprintf(file_move_to, "%s%s", move_dir, ep->d_name);
+                            char *args[] = {"mv", file_to_move, file_move_to, NULL};
+                            execute(args);
+                        }
+                    }
+                }
+            }
+
             terunduh=1;
         }
-        if(lopyu() && terzip==0){
-            hbd();
+        
+        if(cektanggal(0) && terzip==0){
+            // f
+
+            if(1){
+                char *args[] = { "zip", "-r", "Lopyu_Stevany", foldery[0], foldery[1], foldery[2], NULL };
+                execute(args);
+            }
+
+            if(1){
+                char *args[] = {"rm", "-rf", foldery[0], foldery[1], foldery[2], folderbold[0], folderbold[1], folderbold[2], NULL};
+                execute(args);
+            }
+
             terzip=1;
+        }
+        
+        if(terunduh && terzip){
+            exit(EXIT_FAILURE);
         }
         sleep(1);
     }
+    return 0;
 }
